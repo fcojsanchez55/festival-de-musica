da3881b76b590e957bd7c4b24ee266a7d5b93567
jose jorge <developer@iMac-de-jose.local>
2024-07-23 22:36:38 -0400

    Actualizar gulpfile.js para crear el directorio dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,41 @@
 import { src, dest, watch, series, parallel } from 'gulp';
 import * as dartSass from 'sass';
 import gulpSass from 'gulp-sass';
+import clean from 'gulp-clean';
 
 const sass = gulpSass(dartSass);
 
+// Tarea para limpiar la carpeta 'dist'
+export function cleanDist() {
+    return src('dist', { allowEmpty: true, read: false })
+        .pipe(clean());
+}
+
+// Tarea para mover archivos JS al directorio 'dist'
 export function js(done) {
     src('src/js/app.js')
-        .pipe(dest('build/js'));
+        .pipe(dest('dist/js'));
     done();
 }
 
+// Tarea para compilar archivos Sass y moverlos al directorio 'dist'
 export function css(done) {
     src('src/scss/app.scss', { sourcemaps: true })
         .pipe(sass().on('error', sass.logError))
-        .pipe(dest('build/css', { sourcemaps: true }));
+        .pipe(dest('dist/css', { sourcemaps: true }));
     done();
 }
 
+// Tarea para observar cambios y ejecutar las tareas correspondientes
 export function dev() {
     watch('src/scss/**/*.scss', css);
     watch('src/js/**/*.js', js);
 }
 
-// Define the 'build' task
-export const build = series(js, css);
+// Definir la tarea 'build' para limpiar y construir los archivos en 'dist'
+export const build = series(cleanDist, parallel(js, css));
 
+// Exportar por defecto la tarea 'build' seguida de 'dev'
 export default series(build, dev);
 
 
